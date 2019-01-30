7283590ae43311c264a54da709b60325067930e3
Jarosław Weselski <jweselski@objectivity.co.uk>
2019-01-30 15:58:02 +0100

    Add MembershipType Name

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -11,5 +11,6 @@ namespace Vidly.Models
         public short SignupFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
+        public string Name { get; set; }
     }
 }
