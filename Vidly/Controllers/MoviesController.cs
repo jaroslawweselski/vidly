486f74319bceb39ae0fd19dc1922ebeaf99bcca1
Jarosław Weselski <jweselski@objectivity.co.uk>
2019-01-30 17:12:46 +0100

    End of section 3 / Add missing properties to movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -11,6 +12,11 @@ namespace Vidly.Controllers
     {
         private MyDBContext _context;
 
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public MoviesController()
         {
             _context = new MyDBContext();
@@ -18,9 +24,21 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
-            var movies = _context.Movies.ToList();
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
 
             return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(movie);
         }
     }
 }
